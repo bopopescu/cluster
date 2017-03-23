6b9ee294a5fdf490ceb271e36d1251aa2af8df30
hinimix <guanhuai08@163.com>
2017-03-23 20:54:17 +0800

    sql regexp error
    Signed-off-by: hinimix <guanhuai08@163.com>

--- python/log_query/getValue.py
+++ python/log_query/getValue.py
@@ -1,14 +1,22 @@
 import mysql.connector
+
+def fromFiletoDB(file):
+    conn = mysql.connector.connect(user='log', password='1', database='log_query')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS data(IP VARCHAR(15), date_time VARCHAR(30), functions VARCHAR(500));')
+
+    file = open("access.log", 'r')
+    for line in file.readlines():
+        tmp = line.split(" ")
+        # print "INSERT INTO data VALUES(\"%s\", \"%s\", \"%s\");" % (tmp[0], tmp[3].replace('[',''), tmp[6])
+        cursor.execute("INSERT INTO data VALUES(\"%s\", \"%s\", \"%s\");" % (tmp[0], tmp[3].replace('[',''), tmp[6]))
+    conn.commit()
+    conn.close()
+    file.close()
+
+
 conn = mysql.connector.connect(user='log', password='1', database='log_query')
 cursor = conn.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS data(IP VARCHAR(15), date_time VARCHAR(30), functions VARCHAR(500));')
+cursor.execute('')
 
-
-file = open("ttt.log", 'r')
-for line in file.readlines():
-    tmp = line.split(" ")
-    print tmp[0], tmp[3].replace('[',''), tmp[6]
-    cursor.execute('INSERT INTO data VALUES(%s,%s,%s)' % tmp[0], tmp[3].replace('[',''), tmp[6])
-
-conn.commit()
-conn.close()
+select * from data where functions REGEXP '(^common|data|dm|permission|wifi|configdevice|deviceconfig|sp)';
